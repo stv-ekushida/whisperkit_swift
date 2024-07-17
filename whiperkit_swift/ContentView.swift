bc9f5590152924194b61806b7905bf6f7b27efec
e.kushida <e.kushida@stv-tech.co.jp>
2024-07-17 21:31:15 +0900

    Initial Commit

---
+++ whiperkit_swift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  whiperkit_swift
+//
+//  Created by 串田栄二 on 2024/07/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ whiperkit_swift/whiperkit_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  whiperkit_swiftApp.swift
+//  whiperkit_swift
+//
+//  Created by 串田栄二 on 2024/07/17.
+//
+
+import SwiftUI
+
+@main
+struct whiperkit_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
